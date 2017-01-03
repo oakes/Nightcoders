454a741386a7e21f72945e855bb8722c7e0bb0aa
oakes <zsoakes@gmail.com>
2017-01-03 11:45:15 -0600

    Remove react from loading screen

--- src/cljs/nightcoders/loading.cljs
+++ src/cljs/nightcoders/loading.cljs
@@ -1,26 +1,6 @@
 (ns nightcoders.loading
-  (:require [reagent.core :as r]
-            [cljs-react-material-ui.core :refer [get-mui-theme]]
-            [cljs-react-material-ui.reagent :as ui]
-            [nightcoders.auth :as auth])
+  (:require [nightcoders.auth :as auth])
   (:import goog.net.XhrIo))
-
-(defn app []
-  [ui/mui-theme-provider
-   {:mui-theme (get-mui-theme
-                 (doto (aget js/MaterialUIStyles "DarkRawTheme")
-                   (aset "palette" "accent1Color" "darkgray")
-                   (aset "palette" "accent2Color" "darkgray")
-                   (aset "palette" "accent3Color" "darkgray")))}
-   [:span {:style {:margin "auto"
-                   :position "absolute"
-                   :top 0
-                   :left 0
-                   :right 0
-                   :bottom 0
-                   :height 80
-                   :width 80}}
-    [ui/circular-progress {:size 80 :thickness 5}]]])
 
 (defn get-page []
   (.send XhrIo
@@ -36,6 +16,4 @@
                (if (.getBasicProfile user)
                  (auth/auth-user user get-page)
                  (get-page)))))
-
-(r/render-component [app] (.querySelector js/document "#app"))
 
