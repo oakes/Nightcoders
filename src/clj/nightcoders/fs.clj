afd11bd7cda030a59739d4783b32961ad2b01406
oakes <zach@sekao.net>
2017-11-26 20:35:39 -0500

    Update play-cljs

--- src/clj/nightcoders/fs.clj
+++ src/clj/nightcoders/fs.clj
@@ -118,7 +118,7 @@
         (binding [leiningen.new.templates/*dir* (.getCanonicalPath f)]
           (case project-type
             :reagent (gen-project "reagent" '[[reagent "0.7.0"]] project-name main-ns path)
-            :play-cljs (gen-project "play-cljs" '[[play-cljs "0.10.2"]] project-name main-ns path)))
+            :play-cljs (gen-project "play-cljs" '[[play-cljs "0.11.0"]] project-name main-ns path)))
         (-> (InitCommand.)
             (.setDirectory (io/as-file f))
             (.call))
